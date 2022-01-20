e094c7fe53ea1e2cab73472aa07aed5bad4be941
Leonardo <leonardo@ITB001129D.itbam.org.br>
2022-01-20 16:58:17 -0400

    fix: return string to default on App component

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Leonardo King of React
+          Learn React
         </a>
       </header>
     </div>
